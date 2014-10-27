1d8b3f2135719dd51184908d33bf9462aa3550bb
jeyroik <jeyroik@yahoo.com>
2014-10-27 06:14:12 +0300

    Create Fqtl.php

---
+++ library/Fqtl.php
@@ -0,0 +1,29 @@
+<?php
+
+class Fqtl
+{
+    protected $_instance = null;
+
+    protected $_methods = [];
+    
+    protected function __construct(){}
+    
+    public static function e()
+    {
+        return self::$_instance ? self::$_instance : self::$_instance = new self();
+    }
+    
+    public function __call($method, $args)
+    {
+        if(!isset($this->_methods[$method]))
+        {
+            if(is_file(ROOT . $method . '.php'))
+            {
+                $this->_methods[$method] = include ROOT . $method . '.php';
+                return $this->_methods[$method]($args);
+            }
+        }
+        
+        return null;
+    }
+}
